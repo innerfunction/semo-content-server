1985c2015211f78eaeb00bed86ea3247b9dacd47
All views in conf <jloriente@gmail.com>
2016-05-04 12:30:01 +0100

    save file in file, no appending

--- script/git-commands.sh
+++ script/git-commands.sh
@@ -6,8 +6,8 @@ COMMIT="$(git log --pretty=format:%h -n 1 2>&1)"
 echo -e "COMMIT: $COMMIT \n"
 
 
-echo -e "ARCHIVE: $COMMIT \n"
-git archive -o content.zip -v $COMMIT
+#echo -e "ARCHIVE: $COMMIT \n"
+#git archive -o content.zip -v $COMMIT
 
 echo -e "\nSHOW: $COMMIT\n"
 git show --pretty=format: --name-only $COMMIT
