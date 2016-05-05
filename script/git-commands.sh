a6e6385138434d7421452c4d4e7d29013f884f1a
All views in conf <jloriente@gmail.com>
2016-05-05 14:06:19 +0100

    Adding support for current query

--- script/git-commands.sh
+++ script/git-commands.sh
@@ -5,9 +5,19 @@ rm -rf content.*
 COMMIT="$(git log --pretty=format:%h -n 1 2>&1)"
 echo -e "COMMIT: $COMMIT \n"
 
+# used to zip files
+# echo -e "ARCHIVE: $COMMIT \n"
+# git archive -o content.zip -v $COMMIT
 
-#echo -e "ARCHIVE: $COMMIT \n"
-#git archive -o content.zip -v $COMMIT
+# used to see all files in current commit
+# echo -e "\nSHOW: $COMMIT\n"
+# git show --pretty=format: --name-only $COMMIT
 
-echo -e "\nSHOW: $COMMIT\n"
-git show --pretty=format: --name-only $COMMIT
+# git diff --name-status {ref} {current}
+
+# git show --pretty=format: --name-only {commit}
+
+# git ls-tree -r --name-status {commit}:{folder}
+
+
+
